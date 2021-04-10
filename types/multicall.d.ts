84f3c2685dc7acf4d49746d71afb15316348ccb8
Marcelo R. Bianchi <marcelorbianchi96@gmail.com>
2021-04-10 03:53:35 -0300

    revert rename

--- types/multicall.d.ts
+++ types/multicall.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="node"/>
 
-declare module '@bscdelta/multicall' {
+declare module '@makerdao/multicall' {
   import { BigNumber } from 'bignumber.js';
 
   export interface IConfig {
